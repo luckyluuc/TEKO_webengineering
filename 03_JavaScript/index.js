5ef4f67b96431c4782eb0daf77b36cfbd13e076c
luca <lucahuegli@gmail.com>
2023-06-19 19:05:41 +0200

    wip

--- 03_JavaScript/index.js
+++ 03_JavaScript/index.js
@@ -1,9 +1,24 @@
-import {config} from "./config";
-
-document.addEventListener()
+import {config} from "./config.js";
 
 const openRouteApiKey = config.openRouteApiKey;
 const openWeatherApiKey = config.openWeatherApiKey;
+
+const addressInputButton = document.getElementById("addressForm");
+addressInputButton.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let address = document.getElementById("addressInput").value;
+    console.log(address);
+
+    let coordinates = getCoordinates(openRouteApiKey, address);
+    console.log(coordinates);
+
+    let currentWeather = getCurrentWeather(coordinates, openWeatherApiKey);
+    console.log(currentWeather);
+
+    document.getElementById("address").innerHTML = address;
+
+});
 
 function getCoordinates(openRouteApiKey, address) {
     // https://openrouteservice.org/dev/#/api-docs/geocode
@@ -11,10 +26,7 @@ function getCoordinates(openRouteApiKey, address) {
     const url = `https://api.openrouteservice.org/geocode/search?api_key=${openRouteApiKey}&text=${address}`;
 
     fetch(url)
-        .then((response) => {
-            console.log(response.json());
-            return response.json();
-        })
+        .then((response) => response.json())
         .then((data) => {
             const result = data.features[0];
             const latitude = result.geometry.coordinates[1];
@@ -26,28 +38,18 @@ function getCoordinates(openRouteApiKey, address) {
             })
             console.log(coordinates);
             return coordinates;
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        });
 }
 
-function getCurrentWeather(openRouteApiKey, openWeatherApiKey, address) {
-    const coordinates = getCoordinates(openRouteApiKey, address);
+function getCurrentWeather(coordinates, openWeatherApiKey) {
 
     // https://openweathermap.org/current
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${openWeatherApiKey}`;
 
     fetch(url)
-        .then((response) => {
-            console.log(response.json());
-            return response.json();
-        })
+        .then((response) => response.json())
         .then((data) => {
             console.log(data);
             return data;
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        });
 }
