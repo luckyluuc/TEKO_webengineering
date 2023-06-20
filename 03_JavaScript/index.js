130ef151b30b3104cfb5173c21103e354ff92bc9
u238685 <luca.huegli@sbb.ch>
2023-06-20 15:06:33 +0200

    wip

--- 03_JavaScript/index.js
+++ 03_JavaScript/index.js
@@ -10,24 +10,31 @@ addressInputButton.addEventListener("submit", function(event) {
     let address = document.getElementById("addressInput").value;
     console.log(address);
 
-    let coordinates = getCoordinates(openRouteApiKey, address);
-    console.log(coordinates);
-
-    let currentWeather = getCurrentWeather(coordinates, openWeatherApiKey);
-    console.log(currentWeather);
-
-    document.getElementById("address").innerHTML = address;
-
+    getCoordinates(openRouteApiKey, address)
+        .then(coordinates => {
+            console.log(coordinates);
+            return getCurrentWeather(coordinates, openWeatherApiKey);
+        })
+        .then(currentWeather => {
+            console.log(currentWeather);
+            const list = `
+            <ul>
+                <li>${Math.round(currentWeather.main.temp - 273)}°C</li>
+                <li>${currentWeather.main.humidity}%</li>
+                <li>${currentWeather.main.pressure} hPa</li>
+            </ul>
+            `
+            document.getElementById("address").innerHTML = address;
+            document.getElementById("weather").innerHTML = list;
+        });
 });
 
 function getCoordinates(openRouteApiKey, address) {
-    // https://openrouteservice.org/dev/#/api-docs/geocode
-
     const url = `https://api.openrouteservice.org/geocode/search?api_key=${openRouteApiKey}&text=${address}`;
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
             const result = data.features[0];
             const latitude = result.geometry.coordinates[1];
             const longitude = result.geometry.coordinates[0];
@@ -35,20 +42,18 @@ function getCoordinates(openRouteApiKey, address) {
             const coordinates = Object.freeze({
                 lon: longitude,
                 lat: latitude
-            })
+            });
             console.log(coordinates);
             return coordinates;
         });
 }
 
 function getCurrentWeather(coordinates, openWeatherApiKey) {
-
-    // https://openweathermap.org/current
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${openWeatherApiKey}`;
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
             console.log(data);
             return data;
         });
