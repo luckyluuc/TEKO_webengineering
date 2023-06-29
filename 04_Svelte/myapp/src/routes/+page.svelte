6a07c2934f7c6205dd25ba920bc777aa95831119
luca <lucahuegli@gmail.com>
2023-06-29 11:43:01 +0200

    svelte app setup

---
+++ 04_Svelte/myapp/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    h1 {
+        color: darkblue;
+    }
+</style>
